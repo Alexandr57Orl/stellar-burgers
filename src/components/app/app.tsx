ecc4426a74e3a875780a9b116c6be4d444dcb0be
Aleksandr Smetanin <smetanin-sanya2015@yandex.ru>
2024-09-09 23:52:24 +0300

    add Route

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,126 @@
-import { ConstructorPage } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, Modal } from '@components';
+import { ProtectedRoute } from '../routes/ProtectedRoute';
+
+// импорты роутинга по page
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
+
+// импорты модалок
+import { OrderInfo, IngredientDetails } from '@components';
+import {
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+  RouterProvider,
+  createBrowserRouter
+} from 'react-router-dom';
+
+const location = useLocation(); // Инициализируем хук для получения текущего местоположения
+const backgroundLocal = location.state && location.state.background; // Инициализируем хук для получения состояния
+const navigate = useNavigate();
+
+const closeModal = () => {
+  navigate(-1);
+};
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes location={location}>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/ingredients/:id' element={<IngredientDetails />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route
+        path='login'
+        element={
+          <ProtectedRoute>
+            <Login />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='register'
+        element={
+          <ProtectedRoute>
+            <Register />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='forgot-password'
+        element={
+          <ProtectedRoute>
+            <ForgotPassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='reset-password'
+        element={
+          <ProtectedRoute>
+            <ResetPassword />
+          </ProtectedRoute>
+        }
+      />{' '}
+      <Route
+        path='profile'
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='orders'
+        element={
+          <ProtectedRoute>
+            <ProfileOrders />
+          </ProtectedRoute>
+        }
+      />
+      <Route path='*' element={<NotFound404 />} />
+    </Routes>
+    {backgroundLocal && (
+      <Routes>
+        <Route
+          path='/feed/:number'
+          element={
+            <Modal title='' onClose={closeModal}>
+              <OrderInfo />
+            </Modal>
+          }
+        />
+        <Route
+          path='/orders/:number'
+          element={
+            <Modal title='' onClose={closeModal}>
+              <OrderInfo />
+            </Modal>
+          }
+        />
+        <Route
+          path='/ingredients/:id'
+          element={
+            <Modal title='Детали ингредиента' onClose={closeModal}>
+              <OrderInfo />
+            </Modal>
+          }
+        />
+      </Routes>
+    )}
   </div>
 );
 
